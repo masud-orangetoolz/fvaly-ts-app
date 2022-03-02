e3096b3d1752aebcd8134680a2d97ed83c46b521
masud-orangetoolz <masud.rana@orangetoolz.com>
2022-03-02 18:37:29 +0600

    refactor code content

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 function App() {
   const name = "masud";
+  var brother = "brother";
   return (
     <div>
       <h1>Hello, {name}</h1>
       <p>Congratulations for your success on typescript</p>
+      <p>Hope you are well {brother}</p>
     </div>
   );
 }
