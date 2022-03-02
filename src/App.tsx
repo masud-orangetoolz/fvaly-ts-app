4f11cf5e92326088c3df37899b0d86aed2ce99e4
masud-orangetoolz <masud.rana@orangetoolz.com>
2022-03-02 18:30:04 +0600

    first commit my ts project with eslint and prettier setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const name = "masud";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello, {name}</h1>
+      <p>Congratulations for your success on typescript</p>
     </div>
   );
 }
